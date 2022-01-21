c8a50c367068d0e7b87a6514adad237b2b48d142
Rishabh Kumar <rsbkme@gmail.com>
2022-01-21 18:44:53 +0530

    add docs liink to cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,12 @@
 // import Image from "next/image";
 import React from "react";
 
-export const Card = ({ title, description, image }: CardProps) => {
+export const Card = ({ title, description, image, link }: CardProps) => {
   return (
-    <div className="flex flex-col items-start p-6 rounded-2xl w-full dark:bg-[#323545] bg-white text-black dark:text-white">
+    <a
+      href={link}
+      className="flex flex-col items-start p-6 rounded-2xl w-full dark:bg-[#323545] bg-white text-black dark:text-white"
+    >
       <img
         className="items-start"
         width={20}
@@ -13,7 +16,7 @@ export const Card = ({ title, description, image }: CardProps) => {
       />
       <h4 className="text-lg mt-4 font-medium">{title}</h4>
       <p className="mt-2">{description}</p>
-    </div>
+    </a>
   );
 };
 
@@ -21,4 +24,5 @@ export interface CardProps {
   title: string;
   description: string;
   image: string;
+  link: string;
 }
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -6,16 +6,19 @@ const card: CardProps[] = [
     description: "Who we are and what we can do for your business.",
     title: "What is XP.network",
     image: "/assets/info.svg",
+    link: "/docs/home",
   },
   {
     description: "Technological and methodological background of XP.network",
     title: "Whitepaper",
     image: "/assets/file.svg",
+    link: "/docs/home",
   },
   {
     description: "An intuitive interface for integrating the Bridge into dApps",
     title: "JavaScript API",
     image: "/assets/api.svg",
+    link: "/docs/home",
   },
 ];
 
