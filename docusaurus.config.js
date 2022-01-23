9632426bc2dbba6b0dc97b788bb7ef9ff1df5025
Dima Brook <dima@xp.network>
2022-01-23 10:20:23 +0200

    fix editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: "https://github.com/XP-NETWORK/documentation",
+          editUrl: "https://github.com/XP-NETWORK/documentation/blob/main/",
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
