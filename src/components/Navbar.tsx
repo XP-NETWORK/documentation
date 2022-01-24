c6517dda574efe93e5b1e1f74c286db44619c445
Rishabh Kumar <rsbkme@gmail.com>
2022-01-24 14:13:24 +0530

    xp.network to home in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,7 +45,7 @@ export const Navbar = () => {
           className="hidden md:inline dark:hover:text-white"
           href="https://xp.network"
         >
-          XP.NETWORK
+          Home
         </a>
         <a
           className="hidden md:inline dark:hover:text-white"
