71ae95905d584f4ef48e92a43766fe4347cb938f
Dima Brook <dima@xp.network>
2022-02-08 19:49:59 +0200

    change favicon & logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "favicon.ico",
+  favicon: "favicon-new.ico",
   organizationName: "XP.NETWORK", // Usually your GitHub org/user name.
   projectName: "docs.xp.network", // Usually your repo name.
 
