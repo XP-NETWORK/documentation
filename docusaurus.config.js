90bf21656fa134045d1ae215e052aeebffbbaa4b
Dima Brook <dima@xp.network>
2023-06-13 13:46:22 +0300

    add: rust & solidity markup support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -179,6 +179,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        additionalLanguages:['rust', 'solidity']
       },
       themeConfig: {
         colorMode: {
